c96d730c08f18d05c9acefa6d8443b58f74095f5
umetsu <prunusmume0727@gmail.com>
2020-08-18 07:07:38 +0900

    :sparkles: ~2-4 まで

---
+++ ch02/rpn/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    let exp = "6.1 5.2 4.3 * + 3.4 2.5 / 1.6 * -";
+    let ans = rpn(exp);
+    debug_assert_eq!("26.2840", format!("{:.4}", ans));
+    println!("{} = {:.4}", exp, ans);
+}
+
+// RPN形式の文字列expを受け取り、f64型の計算結果を返す
+fn rpn(exp: &str) -> f64 {
+    let mut stack = Vec::new();
+
+    for token in exp.split_ascii_whitespace() {
+        if let Ok(num) = token.parse::<f64>() {
+            stack.push(num);
+        } else {
+            match token {
+                "+" => apply2(&mut stack, |x, y| x + y),
+                "-" => apply2(&mut stack, |x, y| x - y),
+                "*" => apply2(&mut stack, |x, y| x * y),
+                "/" => apply2(&mut stack, |x, y| x / y),
+                _ => panic!("Unknown operator: {}", token),
+            }
+        }
+    }
+    stack.pop().expect("Stack underflow")
+}
+
+// スタックから数値を2つ取り出し、F型のクロージャfunで計算し、結果をスタックに積む
+fn apply2<F>(stack: &mut Vec<f64>, fun: F)
+where
+    F: Fn(f64, f64) -> f64,
+{
+    if let (Some(y), Some(x)) = (stack.pop(), stack.pop()) {
+        let z = fun(x, y);
+        stack.push(z)
+    } else {
+        panic!("Stack underflow");
+    }
+}
